e8ff913e3a766276b9654aebe103ce910462036b
Rafal Pastuszak <rafal.pastuszak@gmail.com>
2018-02-15 16:33:38 +0000

    Enable sourceMaps and UMD support.

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,34 @@
-const path = require('path')
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const path = require( 'path' )
+const UglifyJsPlugin = require( 'uglifyjs-webpack-plugin' )
 
 module.exports = {
     entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'index.js'
+        path: path.resolve( __dirname, 'dist' ),
+        filename: 'index.js',
+        library: 'pse',
+        libraryTarget: 'umd',
+        umdNamedDefine: true,
     },
+    devtool: 'source-map',
     plugins: [
-        new UglifyJsPlugin()
+        new UglifyJsPlugin( {
+            sourceMap: true,
+            cache: true,
+        } ),
     ],
     module: {
         rules: [
-          {
-            test: /\.js$/,
-            exclude: /(node_modules|bower_components)/,
-            use: {
-              loader: 'babel-loader',
-              options: {
-                presets: ['@babel/preset-env']
-              }
-            }
-          }
-        ]
-      }
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [ '@babel/preset-env' ],
+                    },
+                },
+            },
+        ],
+    },
 }
